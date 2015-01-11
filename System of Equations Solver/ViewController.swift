d10b42cad8609bb7aa6a05c4ce10098f1628c909
Anish Chopra <anish.chopra@mail.utoronto.ca>
2015-01-11 01:10:23 -0500

    Initial Commit

---
+++ System of Equations Solver/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  System of Equations Solver
+//
+//  Created by Anish Chopra on 2015-01-11.
+//  Copyright (c) 2015 Anish Chopra. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
